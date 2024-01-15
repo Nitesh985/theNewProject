1a9ad0f89d988f32a6513badae3de8a2e26afc4b
Nitesh Acharya <Niteshacharya796@gmail.com>
2024-01-16 00:07:27 +0545

    product controllers changed

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -1,184 +1,195 @@
 import { Product } from "../models/product.models.js";
 import { Category } from "../models/category.models.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from '../utils/ApiError.js'
+import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import {uploadToCloudinary} from '../utils/cloudinary.js'
-import mongoose from 'mongoose'
+import { uploadToCloudinary } from "../utils/cloudinary.js";
+import mongoose from "mongoose";
 
+const getAllProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find();
 
+  return res.status(200).json({
+    data: products,
+  });
+});
 
+const getProduct = asyncHandler(async (req, res) => {
+  const { productId } = req.params;
 
+  if (!productId) {
+    throw new ApiError(401, "No product id was given");
+  }
 
-const getAllProducts = asyncHandler(async (req, res)=> {
-    const products = await Product.find()
-
-    return res.status(200)
-    .json({
-        data:products
-    })
-})
-
-const getProduct = asyncHandler(async (req, res)=> {
-    const {productId} = req.params
-
-    if (!productId){
-        throw new ApiError(401, "No product id was given")
-    }
-
-
-    const product = await Product.aggregate([
-        {
-            $match:{
-                _id:new mongoose.Types.ObjectId(productId)
-            }
+  const product = await Product.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(productId),
+      },
+    },
+    {
+      $lookup: {
+        from: "categories",
+        localField: "categoryId",
+        foreignField: "_id",
+        as: "category",
+      },
+    },
+    {
+      $addFields: {
+        category: {
+          $first: "$category",
         },
-        {
-            $lookup:{
-                from:"categories",
-                localField: "categoryId",
-                foreignField: "_id",
-                as:"category"
-            },
-        },
-        {    
-            $addFields:{
-                category:{
-                    $first:"$category"
-                }
-            }
-        },
-        {
-            $project: {
-                _id:1,
-                name:1,
-                description:1,
-                images:1,
-                price:1,
-                category:1,
-            }
-        }
-    ])
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        name: 1,
+        description: 1,
+        images: 1,
+        price: 1,
+        category: 1,
+      },
+    },
+  ]);
 
-    if (!product.length){
-        throw new ApiError(500, "Something went wrong searching the current product")
+  if (!product.length) {
+    throw new ApiError(
+      500,
+      "Something went wrong searching the current product"
+    );
+  }
+
+  return res.json({
+    data: product[0],
+    status: 200,
+    message: "The required product is gotten successfully",
+  });
+});
+
+const uploadProduct = asyncHandler(async (req, res) => {
+  const { name, description, price, categoryName } = req.body;
+
+  const reqFields = ["name", "price", "categoryName"];
+
+  reqFields.forEach((field) => {
+    if (!req.body[field]) {
+      throw new ApiError(
+        401,
+        field[0].toUpperCase() + field.slice(1) + " field is a required field"
+      );
     }
+  });
 
-    return res.json({
-        data:product[0],
-        status:200,
-        message:"The required product is gotten successfully"
-    })
+  const category = await Category.findOne({ name: categoryName });
 
+  if (!category) {
+    throw new ApiError(400, "The given category of the product is not found");
+  }
 
-})
+  const imageFiles = req?.files;
 
-const uploadProduct = asyncHandler(async (req, res)=> {
-    const {name, description, price, categoryName} = req.body
-    
-    const reqFields = ['name', 'price', 'categoryName'] 
+  if (!imageFiles) {
+    throw new ApiError(400, "The image for the product is not provided");
+  }
 
+  let images = [];
 
-    reqFields.forEach(field=>{
-        if (!req.body[field]){
-            throw new ApiError(401, ((field[0].toUpperCase() + field.slice(1)) + " field is a required field"))
-        }
-    })
+  for (const image of imageFiles) {
+    const img = await uploadToCloudinary(image.path);
+    images.push(img);
+  }
 
-    const category = await Category.findOne({name:categoryName})
-    
-    if (!category){
-        throw new ApiError(400, "The given category of the product is not found")
-    }
+  if (!images || !images.length || images.length !== imageFiles.length) {
+    throw new ApiError(500, "Something went wrong uploading the image");
+  }
 
-    const imageFiles = req?.files
+  const product = await Product.create({
+    name,
+    description: description || "",
+    price,
+    categoryId: category._id,
+    images,
+  });
 
-    if (!imageFiles){
-        throw new ApiError(400, "The image for the product is not provided")
-    }
+  if (!product) {
+    throw new ApiError(500, "Error while uploading the product");
+  }
 
-    let images = []
-
-    for (const image of imageFiles){
-        const img = await uploadToCloudinary(image.path)
-        images.push(img)
-    }
-
-    if (!images || !images.length || (images.length!==imageFiles.length)){
-        throw new ApiError(500, "Something went wrong uploading the image")
-    }
-
-    const product = await Product.create({
-        name,
-        description:description || "",
-        price,
-        categoryId: category._id,
-        images
-    })
-
-    if (!product){
-        throw new ApiError(500, "Error while uploading the product")
-    }
-
-    return res.json(
-        new ApiResponse(200, product, "The product uploaded successfully")
-    )
-    
-})
-
+  return res.json(
+    new ApiResponse(200, product, "The product uploaded successfully")
+  );
+});
 
 const getReviews = asyncHandler(async (req, res) => {
-    const { productId } = req.params
+  const { productId } = req.params;
 
-    if (!productId){
-        throw new ApiError(404, "No such product exists")
-    }
+  if (!productId) {
+    throw new ApiError(404, "No such product exists");
+  }
 
-    const reviewDetails = await Product.aggregate([
-        {
-            $match: {
-                _id: new mongoose.Types.ObjectId(productId)}
-        },
-        {
-            $lookup:{
-                from: "reviews",
-                localField: "_id",
-                foreignField: "productId",
-                as: "reviewers",
-                pipeline:[
-                    {
-                        from:"customers",
-                        local
+  const reviewDetails = await Product.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(productId),
+      },
+    },
+    {
+      $lookup: {
+        from: "reviews",
+        localField: "_id",
+        foreignField: "productId",
+        as: "reviewers",
+        pipeline: [
+          {
+            $lookup: {
+              from: "customers",
+              localField: "customerId",
+              foreignField: "_id",
+              as: "reviewer",
+              pipeline: [
+                {
+                    $project: {
+                        username:1,
+                        avatar: 1
                     }
-                ]
-            }
-        },
-        {
+                }
+              ]
+            },
+          },
+          {
             $addFields:{
-                reviewCount:{
-                    $size: "$reviewers"
+                reviewer:{
+                    $first: "$reviewer"
                 }
             }
-        }
-    ])
+          }
+        ],
+      },
+    },
+    {
+      $addFields: {
+        reviewCount: {
+          $size: "$reviewers",
+        },
+      },
+    },
+  ]);
 
-    if (!reviewDetails.length){
-        throw new ApiError(500, "Something went wrong getting the review details")
-    }
+  if (!reviewDetails.length) {
+    throw new ApiError(500, "Something went wrong getting the review details");
+  }
 
-    return res
+  return res
     .status(200)
     .json(
-        new ApiResponse(200, reviewDetails[0].reviewers, "The review details were gotten succesfully")
-    )
+      new ApiResponse(
+        200,
+        reviewDetails[0].reviewers,
+        "The review details were gotten succesfully"
+      )
+    );
+});
 
-})
-
-
-
-export {
-    getAllProducts,
-    getProduct,
-    uploadProduct,
-    getReviews
-}
+export { getAllProducts, getProduct, uploadProduct, getReviews };
