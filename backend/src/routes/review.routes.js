1c076dbdfa5aa40551bc711f83f9642fe50a1127
Nitesh <107662492+Nitesh985@users.noreply.github.com>
2024-01-19 17:38:07 +0545

    Update review.routes.js

--- backend/src/routes/review.routes.js
+++ backend/src/routes/review.routes.js
@@ -1,9 +1,10 @@
 import { uploadReview, getReviewsByStars } from "../controllers/review.controller.js"
+import { verifyUser } from "../middlewares/auth.middlewares.js" 
 import { Router } from 'express'
 
 const router = Router()
 
-router.route('upload-review').post(uploadReview)
+router.route('upload-review').post(verifyUser, uploadReview)
 router.route('get-reviews-by-stars').get(getReviewsByStars)
 
 
