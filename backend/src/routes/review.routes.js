31788c3a57c7470e6413d201bfed7b720f21bf73
Nitesh <107662492+Nitesh985@users.noreply.github.com>
2024-01-19 17:33:53 +0545

    Update review.routes.js

--- backend/src/routes/review.routes.js
+++ backend/src/routes/review.routes.js
@@ -1,1 +1,11 @@
+import { uploadReview, getReviewsByStars } from "../controllers/review.controller.js"
+import { Router } from 'express'
+
+const router = Router()
+
+router.route('upload-review').post(uploadReview)
+router.route('get-reviews-by-stars').get(getReviewsByStars)
+
+
+export default router
 
