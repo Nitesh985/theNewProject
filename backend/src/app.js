06260cc3006149e11bf00303e53de54e4b3250b6
Nitesh <107662492+Nitesh985@users.noreply.github.com>
2024-01-19 20:13:43 +0545

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import userRouter from './routes/user.routes.js'
 import productRouter from './routes/product.routes.js'
+import reviewRouter from './routes/review.routes.js'
+import categoryRouter from '.routes/category.routes.js'
 
 
 
@@ -21,6 +23,8 @@ app.use(cookieParser())
 // routes
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/products", productRouter)
+app.use("/api/v1/reviews", reviewRouter)
+app.use("/api/v1/categories", categoryRouter)
 
 
 export default app
